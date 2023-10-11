65da7814e21f7d0ea3677fae4bac8b4175befe64
abhishekchaudhary-github <abhishekch.165@gmail.com>
2023-10-11 17:05:26 +0530

    created

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,4 +1,5 @@
 const Jobs = require('../models/Job')
+const {StatusCodes} = require('http-status-codes')
 
 const get = async (req,res) => {
     res.send(req.user)
@@ -18,4 +19,10 @@ const update = async (req,res) => {
     res.send()
 }
 
-module.exports = {get,getall,del,update}
+const create = async (req,res) => {
+    req.body.createdBy = req.user.userId
+    const response = await Jobs.create(req.body)
+    res.status(StatusCodes.CREATED).json({job : response})
+}
+
+module.exports = {get,getall,del,update,create}
